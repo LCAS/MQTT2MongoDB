94d80373de28d028c01c64182061307d11c9e758
Junior Saldanha <umsaldanha@gmail.com>
2021-08-13 22:05:03 -0300

    hooo gloria, amem irmão, pipeline, bashscript, readme e etc.

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 
 A simple Python project that listen to one or more MQTT topics and save all the incoming messages on a MongoDB database, as some sort of logger.
 
-## Requirements
+# Sections
+0. [MQTT2MongoDB](#mqtt2mongodb)
+1. [Deploy](#deploy)
+    1. [Deploy only MQTT2MongoDB](#deploy-only-mqtt2mongodb)
+    2. [Deploy MQTT2MongoDB MQTT MongoDB](#deploy-mqtt2mongodb-mqtt-mongodb)
+    3. [Debug with docker-compose](#debug-with-docker-compose)
+    4. [Build and run as Daemon without mapping the appsettings file](#build-and-run-as-daemon-without-mapping-the-appsettings-file)
+2. [Requirements](#requirements)
+3. [Settings](#settings)
+4. [Features](#features)
+5. [Document Format in MongoDB](#document-format-in-mongodb)
 
+## Requirements
 - Python 3.x (tested on python:3.8-slim-buster in container)
 - Tested on Docker with docker-compose on Mac OS Big Sur
 - A working, available MQTT broker
@@ -13,12 +24,12 @@ A simple Python project that listen to one or more MQTT topics and save all the 
     * [paho-mqtt](https://pypi.org/project/paho-mqtt/)
 - Docker + docker-compose is recommended if available
 
----
+----
 
 ## Settings
 Settings for MQTT Broker and MongoDB server can be placed on the ***```appsettings.json```*** file.
 
----
+----
 
 ## Features
 - [x] Separated Threads for MQTT and MongoDB
@@ -30,7 +41,7 @@ Settings for MQTT Broker and MongoDB server can be placed on the ***```appsettin
 
 ---
 
-## Format of a Mongo document
+## Document Format in MongoDB
 When payload is a text will be something like
 ```json
 {
@@ -65,9 +76,22 @@ Or when your payload is a json, the document in mongodb will be like below
     "datetime": "2021-08-13T02:29:44.901154"
 }
 ```
----
+----
+# Deploy
+## Deploy only MQTT2MongoDB
 
-## Debug with docker-compose 
+```bash
+curl -sS https://raw.githubusercontent.com/juniorsaldanha/MQTT2MongoDB/master/deploy.sh | bash -s only-mqtt2mongodb
+```
+
+---
+## Deploy MQTT2MongoDB MQTT MongoDB
+```bash
+curl -sS https://raw.githubusercontent.com/juniorsaldanha/MQTT2MongoDB/master/deploy.sh | bash -s full
+```
+
+---
+## Debug with docker-compose
 To Debug with docker container and vscode use the command below, or if you have Docker extension on vscode just right-click on `docker-compose.debug.yml`
 ```bash
 docker-compose -f "docker-compose.debug.yml" up --build -d
@@ -76,7 +100,7 @@ and use "Python: Remote Attach" on Debug option.
 
 ---
 
-## Run as production
+## Build and run as Daemon without mapping the appsettings file
 To run as service on background use the command below
 ```bash
 docker-compose -f "docker-compose.yml" up --build -d
---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, you need to pass one arg 'full' or 'only-mqtt2mongodb'"
+    exit 1
+else
+    if [$1 -eq "full"]
+      then
+        DOCKER_COMPOSE_FILE="docker-compose.full.yml"
+    fi
+    if [$1 -eq "only-mqtt2mongodb"]
+      then
+        DOCKER_COMPOSE_FILE="docker-compose.yml"
+    fi
+fi
+
+
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo "|                            Now we will Download some file to your computer                                |"
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+
+curl -sS https://raw.githubusercontent.com/juniorsaldanha/MQTT2MongoDB/master/.env -o .env_mqtt2mongodb
+source .env_mqtt2mongodb
+
+mkdir $MQTT2MONGODB_PATH
+mv .env_mqtt2mongodb $MQTT2MONGODB_PATH/.env
+cd $MQTT2MONGODB_PATH
+
+curl -sS $MQTT2MONGODB_GITRAW/appsettings.json -o ./appsettings.json
+curl -sS $MQTT2MONGODB_GITRAW/$DOCKER_COMPOSE_FILE -o ./docker-compose.yml
+
+
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo "|  Now we will download the image from Docker Hub, this will take a while, depends on you internet speed   |"
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+
+sudo docker-compose pull --quiet #pulling just to be sure that you have lastest version
+sudo docker-compose up -d
+sudo docker-compose ps
+
+clear
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo "|                 Download of the image is completed, and the container is running                         |"
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo ""
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo "|Change the 'appsettings.json' file to your own configuration, and restart the container with command below|"
+echo "| -->                                     docker-compose restart                                        <--|"
+echo "|                                                                                                          |"
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
+echo "|           Check out my GitHub on https://github.com/juniorsaldanha/MQTT2MongoDB @UmSaldanha              |"
+echo "|_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻_⎻|"
